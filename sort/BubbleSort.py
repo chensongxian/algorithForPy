e0fcfbaf1dd009135c9f9090b07cc4b31d3f1426
chensongxian <1164077611@qq.com>
2017-11-15 00:09:30 +0800

    冒泡

--- sort/BubbleSort.py
+++ sort/BubbleSort.py
@@ -19,26 +19,22 @@ def bubbleSort(arr):
                 arr[i+1]=temp
     return arr
 
-def bubbleSort(arr):
+def bubbleSort_1(arr):
     """冒泡"""
     if arr == None or len(arr)<2:
         return arr
     n = len(arr)
-    forRange = range(1,n)
-    forRange.reverse()
-    print len(arr)
-    print forRange
-    for e in forRange:
+    for i in range(n-1):
         print '----'
-        for i in range(e):
+        for j in range(n-i-1):
             print '(比较:%d-%d)'%(i,i+1)
-            if(arr[i]>arr[i+1]):
-                temp = arr[i]
-                arr[i] = arr[i+1]
-                arr[i+1]=temp
+            if(arr[j]>arr[j+1]):
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1]=temp
     return arr
 if __name__ == '__main__':
     arr = [54,35,48,36,27,12,44,44,8,14,26,17,28]
-    sortArr = bubbleSort(arr)
+    sortArr = bubbleSort_1(arr)
     # sortArr = bubbleSort(arr)
     print sortArr
